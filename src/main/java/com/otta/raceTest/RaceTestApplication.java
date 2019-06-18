1d84e7a1f406395b7d21fc8fb478eb0bb653c666
Guilherme <guilhermeotta@gmail.com>
2019-06-18 20:58:21 -0300

    Ajusta repositório.

---
+++ src/main/java/com/otta/raceTest/RaceTestApplication.java
@@ -0,0 +1,13 @@
+package com.otta.raceTest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RaceTestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RaceTestApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/otta/raceTest/RaceTestApplicationTests.java
@@ -0,0 +1,16 @@
+package com.otta.raceTest;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class RaceTestApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
